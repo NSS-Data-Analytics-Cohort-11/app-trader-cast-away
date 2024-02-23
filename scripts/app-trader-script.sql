1d29a915deb152c061ebf3d7e1786e57649e26d0
larissaemma <emamalie101@gmail.com>
2024-02-22 21:23:04 -0600

    group work

--- scripts/app-trader-script.sql
+++ scripts/app-trader-script.sql
@@ -3,7 +3,7 @@ FROM app_store_apps
 select*
 FROM play_store_apps
 
-
+-- what ages have the most apps 
 SELECT content_rating, count(content_rating)
 FROM app_store_apps
 GROUP BY content_rating
@@ -13,6 +13,7 @@ FROM play_store_apps
 GROUP BY content_rating
 ORDER BY count DESC
 
+-- what genres have the most apps
 SELECT primary_genre, count(primary_genre)
 FROM app_store_apps
 GROUP BY primary_genre
@@ -22,16 +23,59 @@ FROM play_store_apps
 GROUP BY genres
 ORDER BY count DESC
 
-
+--ratings 
 SELECT name, rating, primary_genre, price
 FROM app_store_apps
 WHERE rating >= 4.0
 ORDER BY price DESC
 
-
+--what common apps between both 
 SELECT name 
 FROM app_store_apps
 INTERSECT
 SELECT name
 FROM play_store_apps
+
+Select primary_genre, avg(primary_genre) 
+FROM app_store_apps 
+WHERE primary_genre= 'Games'
+
+--profitability 
+CASE
+ 	WHEN price = 0 THEN ((1*60000)*(1+rating/.5))-((10000) + (12000*(1+rating/.5)))
+ 	ELSE ((1*60000)*(1+rating/.5))-((10000 *price) + (12000*(1+rating/.5)))
+END AS profitability
+
+---second attempt
+
+WITH avg_profit AS (
+  SELECT AVG(CASE
+              WHEN price = 0 THEN ((1*60000)*(1+rating/.5))-((10000) + (12000*(1+rating/.5)))
+              ELSE ((1*60000)*(1+rating/.5))-((10000 *price) + (12000*(1+rating/.5)))
+             END) AS avg_profitability
+  FROM app_store_apps
+),
+profitability AS (
+  SELECT 
+    name,
+    review_count,
+    rating,
+    primary_genre,
+    CASE
+      WHEN price = 0 THEN ((1*60000)*(1+rating/.5))-((10000) + (12000*(1+rating/.5)))
+      ELSE ((1*60000)*(1+rating/.5))-((10000 * price) + (12000*(1+rating/.5)))
+    END AS profitability
+  FROM app_store_apps
+)
+SELECT 
+  name,
+  review_count,
+  rating,
+  primary_genre,
+  profitability
+FROM 
+  profitability
+JOIN avg_profit ON 1=1
+WHERE profitability >= avg_profitability;
+
 
